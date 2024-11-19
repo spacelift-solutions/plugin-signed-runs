4c24cdca569699565f4c0ac44291652c1abea857
apollorion <joey@apollorion.com>
2024-11-19 09:59:45 -0500

    fix: use correct variable

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 locals {
-  stack_ids = [for k, _ in var.access : k]
+  stack_ids = [for k, _ in var.stacks : k]
 
   init_policy_workerpool_userdata = <<-EOT
 echo "${base64encode(templatefile("${path.module}/init_policy.tpl.rego", { SECRET : var.spacelift_run_signature_secret, STACKS : jsonencode(local.stack_ids) }))}" | base64 -d > $HOME/init_policy.rego
